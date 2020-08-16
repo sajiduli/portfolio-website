149cfc1216f7fba7c2991ce6bfd7c40e70937dd7
sajiduli <64068207+sajiduli@users.noreply.github.com>
2020-08-16 07:23:47 -1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// carousel section  ***************************
+$(".carousel").carousel({
+    pause: false,
+    interval: 3000
+});
+// counter up ***************************
+$('.counter').counterUp({
+    delay: 50,
+    time: 3000
+});
